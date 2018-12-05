57d77439e92ff6836331c1096ae46797a00612af
comp1 <chotom73@gmail.com>
2018-12-05 23:16:56 +0900

    output stream name modify

--- src/main/scala/com/yunjae/kafka/streams/StreamStarterApp.scala
+++ src/main/scala/com/yunjae/kafka/streams/StreamStarterApp.scala
@@ -4,10 +4,12 @@ import java.util.Properties
 import java.util.regex.Pattern
 
 import com.lightbend.kafka.scala.streams.{KTableS, StreamsBuilderS}
-import org.apache.kafka.common.serialization.{Serdes}
+import org.apache.kafka.common.serialization.Serdes
 import org.apache.kafka.streams.{KafkaStreams, StreamsConfig}
 import com.lightbend.kafka.scala.streams.ImplicitConversions._
 import com.lightbend.kafka.scala.streams.DefaultSerdes._
+import org.apache.kafka.clients.consumer.ConsumerConfig
+import org.apache.kafka.streams.kstream.Produced
 
 object StreamStarterApp {
   def main(args: Array[String]): Unit = {
@@ -15,29 +17,28 @@ object StreamStarterApp {
       val properties = new Properties()
       properties.put(StreamsConfig.APPLICATION_ID_CONFIG, "wordcount-application")
       properties.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
-      //properties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest")
+      properties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest")
       properties.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass)
       properties.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass)
       properties
     }
 
-    val pattern = Pattern.compile("\\W+", Pattern.UNICODE_CHARACTER_CLASS)
-
     val builder = new StreamsBuilderS()
-    val textLines = builder.stream[String, String]("wordcount-input")
+    val textLines = builder.stream[String, String]("word-count-input")
 
     val wordCounts: KTableS[String, Long] = textLines
-      .flatMapValues(textLines => textLines.toLowerCase.split("\\W+"))
-      .map { (_, value) => (value, value) }
-      //.groupBy((k, v) => v)
+      .flatMapValues(textLines => {
+        println(textLines)
+        textLines.toLowerCase.split(" ")
+      })
+      .selectKey((_, word) => word)
       .groupByKey
       .count("Counts")
 
-
-    wordCounts.toStream.to("wordcount-input")
+    wordCounts.toStream.to("word-count-output")
     val stream = new KafkaStreams(builder.build(), config)
     stream.start()
-    println(stream)
+    println("stream ======== " + stream)
 
     /*Runtime.getRuntime.addShutdownHook(new Thread {
       stream.close()
